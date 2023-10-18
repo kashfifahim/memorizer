d17d7392ce96c0440e3ca5f189cc4d95d98a0085
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-10-18 16:49:03 -0400

    First version of memorization tool

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# memorizer
-A web app to help you practice learning through repetition
+# Memorization Helper
+
+Memorization Helper is a powerful web-based tool designed to enhance and optimize your memorization skills through repetitive typing exercises. Crafted with a user-friendly interface, it encourages learning and memory retention by allowing users to input text lines they wish to memorize and practice them interactively.
+
+## 🌟 Features
+
+- **Custom Text Input**: Add custom lines or paragraphs that you aim to memorize.
+- **Interactive Typing Exercise**: Practice typing the lines, with instant visual feedback.
+- **Progress Tracking**: Keeps track of your memorization progress with a repetition counter.
+
+## 🔧 Installation
+
+1. **Clone the repository**
+git clone https://github.com/kashfifahim/memorizer.git
+
+2. **Navigate to the project directory**
+cd MemorizationHelper
+
+3. **Open `index.html` in your browser**
+- You can double-click the file to run it in your default web browser.
+
+## 🚀 Usage
+
+1. **Enter the Text**: Input the text lines you want to memorize in the provided text area.
+2. **Start Memorizing**: Click on the "Start Memorization" button to begin the exercise.
+3. **Type and Learn**: Type the displayed text, and get instant color-coded feedback.
+
+## 🌐 Deployment
+
+You can deploy the application using platforms like GitHub Pages, Netlify, or Vercel. Follow the documentation on these platforms for guidance on deploying static websites.
+
+## 🤝 Contributing
+
+1. Fork this repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -am 'Add new feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Create a new Pull Request.
+
+## 📜 License
+
+This project is open-source and available under the MIT License.
+
+## 🗣️ Contact
+
+For any queries or suggestions, please feel free to [open an issue](https://github.com/kashfifahim/memorizer/issues) or submit a pull request.
---
+++ app.js
@@ -0,0 +1,66 @@
+document.getElementById('start').addEventListener('click', function() {
+    const inputSection = document.getElementById('input-section');
+    const memorizationSection = document.getElementById('memorization-section');
+    const userInput = document.getElementById('user-input');
+    const targetTextElement = document.getElementById('target-text');
+    const repetitionCounter = document.getElementById('repetition-counter');
+    const nextButton = document.getElementById('next');
+
+    let lines = document.querySelector('textarea').value.split('\n');
+    lines = lines.filter(line => line.trim() !== '');
+
+    console.log(lines);
+
+    if (lines.length === 0) {
+        alert('Please eneter text to memorize.');
+        return;
+    }
+
+    let currentLineIndex = 0;
+    let repetitions = 0;
+
+    function setTargetText() {
+        targetTextElement.textContent = lines[currentLineIndex];
+        repetitionCounter.textContent = `${repetitions+1}/10`;
+        userInput.value = '';
+        userInput.style.color = 'black';
+    }
+
+    function checkInput() {
+        const userInputText = userInput.value;
+        const targetText = lines[currentLineIndex];
+
+        if (userInputText === targetText) {
+            userInput.style.color = 'green';
+            repetitions++;
+
+            if (repetitions >= 10) {
+                repetitions = 0;
+                currentLineIndex++;
+            }
+
+            if (currentLineIndex >= lines.length) {
+                alert('Congratulations! You ahve completed all lines.');
+                location.reload();
+                return;
+            }
+
+            setTimeout(setTargetText, 1000);
+        } else {
+            const isCorrectSoFar = targetText.startsWith(userInputText);
+            userInput.style.color = isCorrectSoFar ? 'black' : 'red';
+        }
+    }
+
+    userInput.addEventListener('input', checkInput);
+    nextButton.addEventListener('click', function() {
+        repetitions = 0;
+        currentLineIndex++;
+        setTargetText();
+    });
+
+    setTargetText();
+    inputSection.hidden = true;
+    memorizationSection.hidden = false;
+    userInput.focus();
+});
